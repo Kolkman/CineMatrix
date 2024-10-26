b6642b26633c35eeeaf9f617425a2111e004c187
Olaf <github@dacht.net>
2024-10-26 08:46:15 +0200

    start with config portal 60 secs

--- include/defaults.h
+++ include/defaults.h
@@ -12,16 +12,18 @@
 
 #define DEFAULT_SPEED 40
 
-#define CONFIGPORTAL_TIMEOUT 10 * 1000 // When configured the Configportal will stay up for a wee bt
+#define CONFIGPORTAL_TIMEOUT 60 * 1000 // When configured the Configportal will stay up for a wee bt
+#define ALWAYS_START_WITH_PORTAL true
 
-#define TEXTLENGTH 256 // Maximum length of text to be displayed
+
+#define RFC952_HOSTNAME "CineMatrix"
 
 #define MAXTEXTELEMENTS 5 // Amount of different sentences to display
 
 // Note you FIELDS have to match what is in LINES
 #define DEFAULTLINE0 "U kijkt naar [[film]]"
 #define DEFAULTLINE1 "De pauze voor deze film is om [[pauze]]"
-#define DEFAULTLINE2 "De film eindigd om [[eind]]"
+#define DEFAULTLINE2 "De film eindigt om [[eind]]"
 #define DEFAULTFIELD0 "film"
 #define DEFAULTFIELD1 "pauze"
 #define DEFAULTFIELD2 "eind"
@@ -29,6 +31,7 @@
 #define DEFAULTVALUE1 "23:30"
 #define DEFAULTVALUE2 "02:00"
 
+#define TEXTLENGTH 256 // Maximum length of text to be displayed
 
 
 #endif
